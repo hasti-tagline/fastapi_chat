24e4e5f62cfcbd394e183a76014258bced081d10
hastinstagline <hastinstaglne@gmail.com>
2026-03-25 11:36:36 +0530

    Initial commit: fastAPI web socket chat

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+# Load frontend
+with open("templates/index.html") as f:
+    html = f.read()
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+clients = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for client in clients:
+                await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
